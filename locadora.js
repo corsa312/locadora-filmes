1e5b8534dd561dad173780bc0c113ee35cff4fe7
Vinicius Otávio <151549965+corsa312@users.noreply.github.com>
2025-09-30 09:31:33 -0300

    Add files via upload

---
+++ locadora.js
@@ -0,0 +1,26 @@
+const statusFilmes = {};
+
+function simularLocacao(filmeId, tipoAcao) {
+  const statusElement = document.getElementById(`status-${filmeId}`);
+  let mensagem = "";
+
+  if (statusFilmes[filmeId] === 1) {
+    mensagem = `ERRO: O filme já foi vendido.`;
+    statusElement.textContent = mensagem;
+    statusElement.classList.add('status-vendido');
+    return;
+  }
+
+  if (tipoAcao === 'Emprestar') {
+    mensagem = `SIMULAÇÃO: "${filmeId}" emprestado por R$ 5,00.`;
+    statusFilmes[filmeId] = 2;
+    statusElement.classList.remove('status-vendido');
+  } else if (tipoAcao === 'Vender') {
+    mensagem = `SIMULAÇÃO: "${filmeId}" vendido por R$ 30,00.`;
+    statusFilmes[filmeId] = 1;
+    statusElement.classList.add('status-vendido');
+  }
+
+  statusElement.textContent = mensagem;
+  alert(mensagem);
+}
